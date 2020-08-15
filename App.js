fc89d03d2ab65d5cf15757c6a26ceebaffba8e70
yuheong <yuhe_ong@hotmail.com>
2020-08-16 00:58:48 +0800

    set default route

--- App.js
+++ App.js
@@ -98,7 +98,7 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Employee">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Employer" component={EmployerScreen} />
         <Stack.Screen name="Employee" component={EmployeeScreen} />
